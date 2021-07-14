a2f87020f9caba36acbd5239c4085b0e084305c4
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-07-14 14:47:09 -0400

    builds product _id page, utilizes the Vuex Store and getters method to access the Route params, adds a default layout to remove our navigation component from the checkout page

--- store/index.js
+++ store/index.js
@@ -218,3 +218,9 @@ export const state = () => ({
     }
   ]
 });
+
+export const getters = {
+  getProductById: state => id => {
+    return state.products.find(product => product.id == id);
+  }
+};
