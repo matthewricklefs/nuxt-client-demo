73e80dea24fa06a3c0008adab2080fc191a64f6c
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-07-14 13:34:55 -0400

    debugs Hero component styles issues, adds SmallCard, SmallCardDisplay, LargeCard, LargeCardDisplay component to begin adding content to our landing page. adds data.js, svg/ directory and images/ directory

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="Hero container">
     <div class="text-container">
-      <h1 class="header">
-        Find your Fire Extinguisher
-      </h1>
+      <h1 class="header">Find your Fire Extinguisher</h1>
       <p class="snippet">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste iusto
         illum quidem asperiores, cum nisi aperiam eius ipsa sapiente odit!
       </p>
       <button class="button btn">Start Looking</button>
-      <img
-        src="~assets/svg/fire-extinguisher.svg"
-        alt="fire extinguisher"
-        class="image"
-      />
     </div>
+    <img
+      class="image"
+      src="~assets/svg/fire-extinguisher.svg"
+      alt="fire extinguisher"
+    />
   </div>
 </template>
 
@@ -31,22 +29,27 @@ export default {};
   align-items: flex-start;
   justify-content: space-between;
 }
+
 .text-container {
   width: 50%;
   margin-top: 4rem;
 }
+
 .header {
   font-weight: 900;
   font-size: 5rem;
 }
+
 .snippet {
   color: grey;
   font-size: 1.25rem;
 }
+
 .image {
   width: 27.5rem;
   margin-top: 2rem;
 }
+
 .overlay {
   background: rgba(0, 0, 0, 0);
   position: absolute;
@@ -54,6 +57,7 @@ export default {};
   height: 100%;
   top: 0;
 }
+
 .button {
   padding: 1rem 4rem;
   border-radius: 100rem;
@@ -63,9 +67,11 @@ export default {};
   font-weight: 700;
   transition: 0.5s;
 }
+
 .button:hover {
   width: 21.5rem;
 }
+
 @media (max-width: 500px) {
   .Hero {
     height: 40vh;
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div class="container">
-    <Hero />
+  <div>
+    <div class="container">
+      <Hero />
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cardsSection="cardInfo"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import { largeCardSections, smallCardSections } from "@/assets/data.js";
+
+export default {
+  data() {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections: smallCardSections
+    };
+  }
+};
 </script>
 
-<style scoped></style>
+<style></style>
